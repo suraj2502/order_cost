cd6e13593b9b80e5466068d9d14f3db3a918cfcd
surajshukla <surajshukla@penguin>
2021-07-16 02:22:40 +0530

    latest changes

--- app/routes/order.py
+++ app/routes/order.py
@@ -49,39 +49,3 @@ async def get_order_items_cost(request:Request,order_details:order_schema):
         200,
         "cost calculated successfully.",
     )
-
-"""
-@order_router.post("/get_items_cost")
-async def get_order_items_cost(request:Request,order_items:List[order_items_schema],distance_details:distance_schema,offer_details:Optional[offer_schema]=None):
-#    if "error" in new_feed_data:
-#        return ErrorResponseModel("An error occurred.", 409, "error")
-    total_cost=0 
-    print(order_items)
-    print(offer_details)
-    print(distance_details.distance)
-    order_items=jsonable_encoder(order_items)
-    print(order_items)
-    for item in order_items:
-        print(item["quantity"])
-        total_cost += item['quantity']*item['price']
-
-    if offer_details==None:
-        return ResponseModel({'total_order_cost':total_cost},200,"cost calculated without any offer" )
-
-    if offer_details.offer_type == "FLAT":
-        total_cost += delivery_cost(distance_details.distance)
-
-        total_cost = max(total_cost - offer_details.offer_value,0)
-    elif offer_details.offer_type == "DELIVERY":
-        pass
-    else:
-        total_cost += delivery_cost(distance_details.distance)
-
-    response={'total_order_cost':total_cost }
-    return ResponseModel(
-        response,
-        200,
-        "cost calculated successfully.",
-    )
-"""
- 
